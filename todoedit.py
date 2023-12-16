7b108540d9d434b6436b3d7d076967ef8701c459
satishchandrak <satishchandra745@gmail.com>
2023-12-16 17:27:03 -0600

    enumarate

--- todoedit.py
+++ todoedit.py
@@ -1,6 +1,6 @@
 itlis = []
 while True:
-    usrin=input("Enter add show edit or exit to your to do list:")
+    usrin=input("Enter add show edit delete or exit to your to do list:")
     match usrin:
        case 'add':
            it=input("Enter the item you want to add:")
@@ -8,7 +8,7 @@ while True:
            itlis.append(it)
        case 'show':
            for indexes, names in enumerate(itlis):
-               print(f"{indexes}-{names}")
+               print(f"{indexes+1}-{names}")
            #itlis=[i for i in itlis]
            #print(itlis)
            #print(*itlis,sep='\n')
@@ -16,6 +16,11 @@ while True:
            rep=int(input("which item do you want to edit? say in number"))
            itlis[rep-1]=input("enter new item")
            print("your to do now:",*itlis,sep='\n')
+       case 'delete':
+           rem=int(input("which item do you want to delete? say in number"))
+           itlis.pop(rem-1)
+           for indexes, names in enumerate(itlis):
+               print(f"{indexes+1}-{names}")
        case 'exit':
             print("good bye")
             break
