3c16be81043130ab39c7e89718a52f92bbf50207
satishchandrak <satishchandra745@gmail.com>
2023-12-16 17:01:39 -0600

    todolist first commit

---
+++ todoedit.py
@@ -0,0 +1,19 @@
+itlis = []
+while True:
+    usrin=input("Enter add show edit or exit to your to do list:")
+    match usrin:
+       case 'add':
+           it=input("Enter the item you want to add:")
+
+           itlis.append(it)
+       case 'show':
+           itlis=[i for i in itlis]
+           print(itlis)
+           #print(*itlis,sep='\n')
+       case 'edit':
+           rep=int(input("which item do you want to edit? say in number"))
+           itlis[rep-1]=input("enter new item")
+           print("your to do now:",*itlis,sep='\n')
+       case 'exit':
+            print("good bye")
+            break
